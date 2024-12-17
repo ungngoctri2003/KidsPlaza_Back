2669ed41e37a5dc5afe76f4a90e1649544d7f595
lamcongtri2003 <lamcongtri2003@gmail.com>
2024-12-17 11:30:11 +0700

    update

--- connection_db.js
+++ connection_db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize("api_pet", "root", null, {
+const sequelize = new Sequelize("api_laptop", "root", null, {
   host: "localhost",
   dialect: "mysql",
   logging: false,
